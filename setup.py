826c30a4587411ab7a5b834b93e03f831134b09f
FlamtaiL <114789259+Cobkgukgg@users.noreply.github.com>
2026-02-11 22:11:58 +0700

    Update author information and repository URL

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name="neuralforge",
     version="1.0.0",
-    author="Your Name",
-    author_email="your.email@example.com",
+    author="Cobkgukgg",
+    author_email="nb652990@gmail.com",
     description="A state-of-the-art neural network framework built from scratch",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/neuralforge",
+    url="https://github.com/Cobkgukgg/neuralforge",
     packages=find_packages(),
     classifiers=[
         "Development Status :: 4 - Beta",
